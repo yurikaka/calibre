4a453f22ef5ed725110790e57069ef1e4b3a3536
Kovid Goyal <kovid@kovidgoyal.net>
2020-06-11 08:47:33 +0530

    Code to read and write annotations to the calibre db

--- src/calibre/gui2/viewer/integration.py
+++ src/calibre/gui2/viewer/integration.py
@@ -7,10 +7,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import re
 
-from polyglot.functools import lru_cache
 
-
-@lru_cache(maxsize=2)
 def get_book_library_details(absolute_path_to_ebook):
     absolute_path_to_ebook = os.path.abspath(os.path.expanduser(absolute_path_to_ebook))
     base = os.path.dirname(absolute_path_to_ebook)
@@ -24,3 +21,37 @@ def get_book_library_details(absolute_path_to_ebook):
     if not os.path.exists(dbpath):
         return
     return {'dbpath': dbpath, 'book_id': book_id, 'fmt': absolute_path_to_ebook.rpartition('.')[-1].upper()}
+
+
+def load_annotations_map_from_library(book_library_details):
+    import apsw
+    from calibre.db.backend import annotations_for_book, Connection
+    ans = {}
+    dbpath = book_library_details['dbpath']
+    try:
+        conn = apsw.Connection(dbpath, flags=apsw.SQLITE_OPEN_READONLY)
+    except Exception:
+        return ans
+    try:
+        conn.setbusytimeout(Connection.BUSY_TIMEOUT)
+        for annot in annotations_for_book(conn.cursor(), book_library_details['book_id'], book_library_details['fmt']):
+            ans.setdefault(annot['type'], []).append(annot)
+    finally:
+        conn.close()
+    return ans
+
+
+def save_annotations_list_to_library(book_library_details, alist):
+    import apsw
+    from calibre.db.backend import save_annotations_for_book, Connection
+    dbpath = book_library_details['dbpath']
+    try:
+        conn = apsw.Connection(dbpath, flags=apsw.SQLITE_OPEN_READWRITE)
+    except Exception:
+        return
+    try:
+        conn.setbusytimeout(Connection.BUSY_TIMEOUT)
+        with conn:
+            save_annotations_for_book(conn.cursor(), book_library_details['book_id'], book_library_details['fmt'], alist)
+    finally:
+        conn.close()
